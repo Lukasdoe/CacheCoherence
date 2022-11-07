3ced6343e19cd4af95d3fd3d8e9ed17d8daa8ec5
Lukas Döllerer <info@lukas-doellerer.de>
2022-11-07 16:24:24 +0800

    combine figures into one horizontal

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,8 +1,7 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import os
 
+import matplotlib.pyplot as plt
+import pandas as pd
 
 # legend = ["1024 B", "4096 B", "8192 B"]
 
@@ -16,7 +15,11 @@ plt.style.use(style_path)
 def cycles_same(df, field, legend, advanced=False):
     dfs = []
     inputs = ["blackscholes_four.zip", "bodytrack_four.zip", "fluidanimate_four.zip"]
-    cond = (df["protocol"] != "Dragon") if advanced else (df["protocol"] != "Mesi (advanced)")
+    cond = (
+        (df["protocol"] != "Dragon")
+        if advanced
+        else (df["protocol"] != "Mesi (advanced)")
+    )
     for file in inputs:
         df_file = (
             df[(df["input"] == file) & cond]
@@ -47,7 +50,11 @@ def cycles_same(df, field, legend, advanced=False):
 def cycles(df, field, legend, advanced=False):
     dfs = []
     inputs = ["blackscholes_four.zip", "bodytrack_four.zip", "fluidanimate_four.zip"]
-    cond = (df["protocol"] != "Dragon") if advanced else (df["protocol"] != "Mesi (advanced)")
+    cond = (
+        (df["protocol"] != "Dragon")
+        if advanced
+        else (df["protocol"] != "Mesi (advanced)")
+    )
     for file in inputs:
         df_file = (
             df[(df["input"] == file) & cond]
@@ -58,25 +65,60 @@ def cycles(df, field, legend, advanced=False):
         dfs.append(df_file)
 
     names = ["blackscholes", "bodytrack", "fluidanimate"]
-    legend = ["Mesi", "Mesi (advanced)"] if advanced else ["Dragon", "Mesi"]
+    legend = (
+        ["Mesi", "Mesi (advanced)"] if advanced else ["Dragon", "Mesi", "Difference"]
+    )
     for i in range(3):
-        fig, ax = plt.subplots()
-        dfs[i].plot.bar(rot=0, ax=ax, alpha=0.7, color=["red", "orange", "olive"])
-        ax.set_ylabel(r"Cycles")
-        ax.set_xlabel(r"")
-        ax.legend(legend, loc="upper center", bbox_to_anchor=(0.5, -0.08), ncol=3)
+        fig, (ax1, ax2) = plt.subplots(2)
+        ax1.axhline(y=0, color="black", linestyle="-")
+
+        cpy = dfs[i].copy()["total_cycles"]
+        cpy["difference"] = cpy["Mesi"] - cpy["Dragon"]
+        cpy["difference"].plot.bar(rot=0, ax=ax1, alpha=0.7, color=["olive"])
+        ax1.set_ylabel(r"Cycles")
+        ax1.set_xlabel(r"")
+        ax1.legend([])
+        ax1.set_ybound(-300000, 300000)
+
+        dfs[i].plot.bar(rot=0, ax=ax2, alpha=0.7, color=["red", "orange", "olive"])
+        ax2.set_ylabel(r"Cycles")
+        ax2.set_xlabel(r"")
+        ax2.legend([])
+
+        handles, _ = ax1.get_legend_handles_labels()
+        handles2, _ = ax2.get_legend_handles_labels()
+        h = handles + handles2
+        fig.legend(
+            labels=legend,
+            handles=h[0:],
+            loc="upper center",
+            bbox_to_anchor=(0.5, 0),
+            fancybox=True,
+            shadow=True,
+            ncol=5,
+        )
         advanced_name = "_advanced" if advanced else ""
         out = os.path.join(out_path, f"{field}_{names[i]}{advanced_name}.svg")
-        plt.savefig(out)
+        plt.savefig(out, bbox_inches="tight")
         plt.show()
 
 
+# Ref: https://matplotlib.org/stable/gallery/subplots_axes_and_figures/subplots_demo.html
 def custom(df, field, advanced=False):
     dfs = []
     inputs = ["blackscholes_four.zip", "bodytrack_four.zip", "fluidanimate_four.zip"]
-    cond = (df["protocol"] != "Dragon") if advanced else (df["protocol"] != "Mesi (advanced)")
+    cond = (
+        (df["protocol"] != "Dragon")
+        if advanced
+        else (df["protocol"] != "Mesi (advanced)")
+    )
     for file in inputs:
-        df_file = df[cond].groupby(["protocol", "input"])[[field]].mean().unstack("input", fill_value=0)
+        df_file = (
+            df[cond]
+            .groupby(["protocol", "input"])[[field]]
+            .mean()
+            .unstack("input", fill_value=0)
+        )
         dfs.append(df_file)
 
     names = ["blackscholes", "bodytrack", "fluidanimate"]
@@ -93,7 +135,7 @@ def custom(df, field, advanced=False):
         ax.set_xlabel(r"")
         ax.legend(legend, loc="upper center", bbox_to_anchor=(0.5, -0.08), ncol=2)
         advanced_name = "_advanced" if advanced else ""
-        out = os.path.join(out_path, f"{field}{advanced_name}.svg")
+        out = os.path.join(out_path, f"{field}{advanced_name}.pdf")
         plt.savefig(out)
         plt.show()
 
@@ -137,8 +179,8 @@ def invalidations(advanced=False):
 
 
 if __name__ == "__main__":
-    # cache_size(True)
-    block_size(True)
-    associativity(True)
+    cache_size(False)
+    # block_size(True)
+    # associativity(True)
     # bus_traffic()
     # invalidations()
